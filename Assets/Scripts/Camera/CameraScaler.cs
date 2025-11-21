b8989b402e45cd0bddf4b39072ab4ac54c8968b5
z0te11 <zotetop@gmail.com>
2025-11-21 16:12:10 +0300

    v0.005

--- Assets/Scripts/Camera/CameraScaler.cs
+++ Assets/Scripts/Camera/CameraScaler.cs
@@ -4,18 +4,37 @@ namespace GAMEPLAY
 {
     public class CameraScaler : MonoBehaviour
     {
+        [Header("Camera Settings")]
+        [SerializeField] private float _referenceHeight = 1920f;
+        [SerializeField] private float _referenceWidth = 1080f;
         [SerializeField] private float _baseSize = 5f;
+        
+        private Camera _camera = null;
 
         private void Awake()
         {
+            _camera = GetComponent<Camera>();
             ScaleCamera();
         }
 
         private void ScaleCamera()
         {
-            Camera cam = GetComponent<Camera>();
-            float screenHeight = Screen.height;
-            cam.orthographicSize = _baseSize * (1080f / screenHeight);
+            if (_camera == null)
+            {
+                return;
+            }
+
+            float screenAspect = (float)Screen.width / Screen.height;
+            float referenceAspect = _referenceWidth / _referenceHeight;
+            
+            if (screenAspect >= referenceAspect)
+            {
+                _camera.orthographicSize = _baseSize;
+            }
+            else
+            {
+                _camera.orthographicSize = _baseSize * (referenceAspect / screenAspect);
+            }
         }
     }
 }
